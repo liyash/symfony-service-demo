aa06d3ba5a211ea86584d679f61cd3f32a283cad
liyashereef <liyashereef@gmail.com>
2025-05-29 14:50:54 +0530

    To Availio

--- src/AvailioBundle.php
+++ src/AvailioBundle.php
@@ -2,7 +2,13 @@
 namespace Availio;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Override;
 
 class AvailioBundle extends Bundle
 {
+    #[Override]
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
 }
