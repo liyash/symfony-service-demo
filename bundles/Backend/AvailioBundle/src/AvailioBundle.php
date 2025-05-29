2ed73eea4640d378c538e8bb60de6c20153d1913
liyashereef <liyashereef@gmail.com>
2025-05-29 11:31:20 +0530

    submodule-repo

---
+++ bundles/Backend/AvailioBundle/src/AvailioBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Backend\AvailioBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AvailioBundle extends Bundle
+{
+}
--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,15 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function registerBundles(): array
+    {
+        $bundles = [
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            // Other bundles
+            new \Backend\AvailioBundle\AvailioBundle(),
+        ];
+        
+        return $bundles;
+    }
 }
